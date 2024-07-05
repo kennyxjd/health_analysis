e087b3a5ddda9a26dff79b3dac7a9c746dcae7f4
jiangdong.xia <jiangdong.xia@bthome.com>
2024-07-05 23:37:07 +0800

    init

--- main.py
+++ main.py
@@ -10,6 +10,44 @@ from typing import Callable
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
+class CalNameHealth:
+    def __init__(self, file_path):
+        self.health_name_dict = self.load_csv_name_files(file_path)
+
+    def load_csv_name_files(self, file_path):
+        health_name_dict = {}
+        df = pd.read_csv(file_path, skiprows=1)
+        for _, row in df.iterrows():
+            health_name_dict[row.iloc[1]] = {'name': row.iloc[0],
+                                             'gender': row.iloc[2],
+                                             'age': row.iloc[3],
+                                             'height': row.iloc[4],
+                                             'weight': row.iloc[5],
+                                             'weight_status': row.iloc[6],
+                                             'eye': row.iloc[7],
+                                             'eye_status': row.iloc[8]}
+        return health_name_dict
+
+    def check_info(self, name):
+        result = ''
+        for id in self.health_name_dict.keys():
+            name_temp = self.health_name_dict[id]['name']
+            if name_temp == name:
+                gender = self.health_name_dict[id]['gender']
+                age = self.health_name_dict[id]['age']
+                height = self.health_name_dict[id]['height']
+                weight = self.health_name_dict[id]['weight']
+                weight_status = self.health_name_dict[id]['weight_status']
+                eye = self.health_name_dict[id]['eye']
+                eye_status = self.health_name_dict[id]['eye_status']
+                info = f"姓名：{name} 学号：{id} 性别：{gender} 年龄：{age} 身高：{height} 体重：{weight} 体重情况：{weight_status} " \
+                       f"视力：{eye} 视力状态：{eye_status} \n"
+                result += info
+        if result == '':
+            return '没有查询到该学生'
+        else:
+            return result
+
 class CalHealth:
     def __init__(self, folder_path) -> None:
         self.range = dict()
@@ -102,6 +140,7 @@ class CalHealth:
 
 app = FastAPI()
 calhealth = CalHealth("./data/health")
+calhealthname = CalNameHealth("./data/stu_info/health_name.csv")
 
 class HealthCheckRequest(BaseModel):
     age: int
@@ -109,12 +148,34 @@ class HealthCheckRequest(BaseModel):
     gender: str
     weight: float
 
+class HealthNameCheckRequest(BaseModel):
+    name: str
+
 # @app.middleware("http")
 # async def log_request_middleware(request: Request, call_next: Callable):
 #     body = await request.body()
 #     logger.info(f"Request body: {body.decode('utf-8')}")
 #     response = await call_next(request)
 #     return response
+
+@app.post("health-name-check")
+async def health_name_check(request: HealthNameCheckRequest):
+    logger.info("=" * 70)
+    logger.info(f"Received health-name check request: {request}")
+    try:
+        result = calhealthname.check_info(
+            name=request.name)
+
+        if result == "No matching health result found.":
+            logger.warning(f"No matching health result found for request: {request}")
+            raise HTTPException(status_code=404, detail=result)
+        logger.info(f"Health check result: {result}")
+        return JSONResponse(content={"health_status": result})
+
+    except ValidationError as e:
+        logger.error(f"Validation error: {e}")
+        logger.error(f"Validation error details: {e.errors()}")
+        raise HTTPException(status_code=422, detail=e.errors())
 
 @app.post("/health-check")
 async def health_check(request: HealthCheckRequest):
